43aecfd14cdbaf4931c268496c77f83d79ab6aa8
Aaron Kao <aaron.kao@live.ca>
2019-08-22 18:18:11 -0700

    small updates. doesnt really work...

--- src/components/Add/Add.jsx
+++ src/components/Add/Add.jsx
@@ -3,12 +3,29 @@ import { Button } from "reactstrap";
 import Modal from "../Modal/Modal";
 
 class Add extends React.Component {
-  state = { show: false };
+  state = { show: false, users: 0 };
 
   showModal = () => {
     this.setState({
       ...this.state,
       show: !this.state.show
+    });
+  };
+
+  handleSubmit = e => {
+    console.log(this);
+    e.preventDefault();
+    this.setState({ users: this.state.users + 1 });
+
+    this.showModal();
+  };
+
+  addUser = () => {
+    console.log(this);
+    this.setState(prevState => {
+      return {
+        users: (prevState.users += 1)
+      };
     });
   };
 
@@ -19,8 +36,16 @@ class Add extends React.Component {
           +
         </Button>
 
+        <button onClick={this.addUser}>Add User</button>
+        {this.state.users}
         <Modal show={this.state.show} onClose={this.showModal}>
-          Works!
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label>Price</label>
+              <input type="text" />
+              <button type="submit">Submit</button>
+            </div>
+          </form>
         </Modal>
       </div>
     );

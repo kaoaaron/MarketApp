87bf508e4d2be6907c67830ac53aaac668ae6fe3
jueun610 <jueun9389@gmail.com>
2019-08-26 13:41:15 -0700

    added google fonts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Add from "./components/Add/Add";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Modal from "./components/Modal/Modal";
+import NavBar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <React.Fragment>
+      <NavBar />
       <Add />
+      <Login />
       <Modal />
+      <Footer />
     </React.Fragment>
   );
 }
